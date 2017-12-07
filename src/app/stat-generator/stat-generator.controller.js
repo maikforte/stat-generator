308f65fc0cca4ce222625e15d3f95b74a4d56630
Michael 'Maik' Ardan <michael_ardan2000@yahoo.com>
2017-12-07 14:54:06 +0800

    ALPHA v1.0.0

--- src/app/stat-generator/stat-generator.controller.js
+++ src/app/stat-generator/stat-generator.controller.js
@@ -68,6 +68,7 @@ angular.module("Dota2StatGenerator")
                         FB.ui({
                             method: 'share_open_graph',
                             action_type: 'og.shares',
+                            display: "page",
                             action_properties: JSON.stringify({
                                 object: {
                                     'fb:app_id': '1615955601781169',
