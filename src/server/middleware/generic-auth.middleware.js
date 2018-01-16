f90adeea9d956ed7980d9a27c71b9413e675fcbd
Michael 'Maik' Ardan <michael_ardan2000@yahoo.com>
2018-01-16 15:07:54 +0800

    Added printer for origin

--- src/server/middleware/generic-auth.middleware.js
+++ src/server/middleware/generic-auth.middleware.js
@@ -1,4 +1,5 @@
 module.exports.authenticate = function (req, res, next) {
+    console.log("origin", req.headers.origin);
     if (req.headers.origin != 'http://www.vertigoo.org') {
         res.status(401);
         res.send("Unauthorized");
